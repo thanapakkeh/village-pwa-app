a2835aefa1fc3002ecdbe930287f803228c1c077
thanapakkeh <thanapakkeh@gmail.com>
2025-03-15 17:22:04 +0700

    Update main.js

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ window.searchByHouseNumber = async function (houseNumber) {
           padding: 20px;
           box-shadow: 0 4px 12px rgba(0,0,0,0.06);
           text-align: left;
-          font-size: 2.0rem;
+          font-size: 1.3rem;
           line-height: 1.6;
         ">
           <p>📅 <strong>ช่วงค้างชำระ:</strong> ${matchWithData["ช่วงค้างชำระ"]}</p>
