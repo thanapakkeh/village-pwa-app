dc8e6307053352b76c17d805ca45fb4325790d57
thanapakkeh <thanapakkeh@gmail.com>
2025-03-16 22:45:16 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,1 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD1e3LrEGStfFOvSr-Di5oBgQQPnvJtU-g",
-  authDomain: "village-app-efa90.firebaseapp.com",
-  projectId: "village-app-efa90",
-  storageBucket: "village-app-efa90.firebasestorage.app",
-  messagingSenderId: "351880359365",
-  appId: "1:351880359365:web:489c4a605e748a17cbafac",
-  measurementId: "G-WDPLXCKY31"
-};
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-export { app, analytics };
